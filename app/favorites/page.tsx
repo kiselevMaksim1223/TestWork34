f11607899645b6fbaecc8548567d70104e38a1fb
Maksim Kiselev <kiselev.maksim1223@gmail.com>
2025-04-19 20:39:07 +0400

    add metadata for favorites page

--- app/favorites/page.tsx
+++ app/favorites/page.tsx
@@ -1,5 +1,10 @@
 import { FavoritesLocations } from '@widgets/favorite-locations'
 
+export const metadata = {
+  title: 'Favorites',
+  description: 'Your favorite locations'
+}
+
 export default function FavoritesPage() {
   return <FavoritesLocations />
 }
